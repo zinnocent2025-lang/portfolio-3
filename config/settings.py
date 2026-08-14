fa1443ff4fb7cae2b1478240e935161ede32b7f9
Zinnocent <zinnocent2025@gmail.com>
2026-08-14 17:14:18 +0100

    Use simpler WhiteNoise storage

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STORAGES = {
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
     },
 }
 
