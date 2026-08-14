b2f82cc1e7e2ebea7409b36d48d68a410f904b39
Zinnocent <zinnocent2025@gmail.com>
2026-08-14 17:07:24 +0100

    Fix static files for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -125,6 +125,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STORAGES = {
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
